3151689e3113ea62c2fca253daa3173e392104fb
divdonis <38712761+divdonis@users.noreply.github.com>
2018-05-29 12:53:02 -0600

    Add files via upload

---
+++ photos.js
@@ -0,0 +1,33 @@
+$("img").click(function(){
+	$(this).toggleClass("expand");
+});
+
+down vote
+$(document).ready(function() {
+    // Fix background image caching problem
+    if (jQuery.browser.msie) {
+        try { 
+            document.execCommand("BackgroundImageCache", false, true); 
+        } catch(err) {}
+    }
+};
+// $(function() {
+//     $('div').on("click", "img", function() {
+//        // alert();
+//          if($(this).hasClass("set"))
+//         {
+//            // alert();
+//          $(this).fadeOut(1000, function() { 
+//            $(this).toggleClass('expand');
+//         });
+          
+//         } else {
+            
+//             //alert('no class');
+//         $(this).fadeIn(1000, function() { 
+//            $(this).addClass('set'); 
+//         });
+//         }
+
+//     });
+// });
